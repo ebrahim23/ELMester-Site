a2bf1f8af4141717563429358eef37b926fa1e50
Ibrahim Khalid <43196067+ebrahim23@users.noreply.github.com>
2020-05-03 19:10:21 +0300

    done

--- js/elmester.js
+++ js/elmester.js
@@ -1,9 +1,21 @@
-let landPage = document.querySelector(".land-page");
+let icon = document.querySelector('.setting i').onclick = function(){
+    this.classList.toggle('fa-spin');
+    document.querySelector('.setting').classList.toggle('open');
+}
+/////////////////////////////////////////////////////////////////////////////////
+const colors = document.querySelectorAll('.uls li');
 
-let arrayImgs = ['../imgs/1.png', '../imgs/2.png', '../imgs/3.png'];
+colors.forEach(li => {
+    li.addEventListener('click', function(e){
+        this.classList.add('active');
+        document.documentElement.style.setProperty('--main-color', e.target.dataset.color);
+    });
+});
+/////////////////////////////////////////////////////////////////////////////////
+let landPage = document.querySelector(".land-page");
+let arrayImgs = ['1.png', '2.png', '3.png'];
 
 setInterval(() => {
     let randomNum = Math.floor(Math.random() * arrayImgs.length);
-
-    landPage.style.backgroundImage = 'url("../imgs/' + arrayImgs[randomNum] + '")';
+    landPage.style.backgroundImage = 'url("imgs/' + arrayImgs[randomNum] + '")';
 }, 5000)
